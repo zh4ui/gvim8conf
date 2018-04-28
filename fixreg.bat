d28569520a47e730de1092eae393719f06648ab5
zh4ui <zh4ui@users.noreply.github.com>
2018-04-28 16:37:04 +0800

    fix regisry

---
+++ fixreg.bat
@@ -0,0 +1,7 @@
+
+@rem for reg command, see https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc742162(v=ws.11)
+
+reg add HKCU\Software\Classes\*\shell\Vim /f /d "Edit with &Vim Tab"
+reg add HKCU\Software\Classes\*\shell\Vim\command /f /d "\"%APPDATA%\vim\vim80\gvim.exe\" -p --remote-tab-silent \"%%1\" \"%%*\""
+
+@pause
