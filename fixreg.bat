b4f4fbdc30df79973a60355c269d8800834edb7f
zh4ui <zh4ui@users.noreply.github.com>
2018-05-21 09:37:51 +0800

    update for vim81

--- fixreg.bat
+++ fixreg.bat
@@ -2,6 +2,6 @@
 @rem for reg command, see https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc742162(v=ws.11)
 
 reg add HKCU\Software\Classes\*\shell\Vim /f /d "Edit with &Vim Tab"
-reg add HKCU\Software\Classes\*\shell\Vim\command /f /d "\"%APPDATA%\vim\vim80\gvim.exe\" -p --remote-tab-silent \"%%1\" \"%%*\""
+reg add HKCU\Software\Classes\*\shell\Vim\command /f /d "\"%APPDATA%\vim\vim81\gvim.exe\" -p --remote-tab-silent \"%%1\" \"%%*\""
 
 @pause
